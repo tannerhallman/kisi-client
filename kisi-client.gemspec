80176528f8b3ce762aa5188886a5b52b70cf4f1c
Tanner Hallman <tanner@lithiosapps.com>
2019-09-29 15:55:04 -0400

    Added http-client

--- kisi-client.gemspec
+++ kisi-client.gemspec
@@ -1,25 +1,26 @@
 # coding: utf-8
-lib = File.expand_path('../lib', __FILE__)
+lib = File.expand_path("../lib", __FILE__)
 $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
-require 'kisi/client/version'
+require "kisi/client/version"
 
 Gem::Specification.new do |spec|
-  spec.name          = "kisi-client"
-  spec.version       = Kisi::Client::VERSION
-  spec.authors       = ["Yevhen V."]
-  spec.email         = ["wik@bdhr.co"]
+  spec.name = "kisi-client"
+  spec.version = Kisi::Client::VERSION
+  spec.authors = ["Tanner H."]
+  spec.email = ["tanner@coworksapp.com"]
 
-  spec.summary       = %q{Ruby Client for the KISI (Access Control System) API}
-  spec.homepage      = "https://github.com/yevgenko/kisi-client"
-  spec.license       = "MIT"
+  spec.summary = %q{Ruby Client for the KISI (Access Control System) API}
+  spec.homepage = "https://github.com/tannerhallman/kisi-client"
+  spec.license = "MIT"
 
-  spec.files         = `git ls-files -z`.split("\x0").reject { |f| f.match(%r{^(test|spec|features)/}) }
-  spec.bindir        = "exe"
-  spec.executables   = spec.files.grep(%r{^exe/}) { |f| File.basename(f) }
+  spec.files = `git ls-files -z`.split("\x0").reject { |f| f.match(%r{^(test|spec|features)/}) }
+  spec.bindir = "exe"
+  spec.executables = spec.files.grep(%r{^exe/}) { |f| File.basename(f) }
   spec.require_paths = ["lib"]
 
-  spec.add_runtime_dependency 'httparty', '~> 0.13.7'
+  spec.add_runtime_dependency "httparty", "~> 0.16.3"
+  spec.add_runtime_dependency "rest-client", "~> 2.1.0"
 
-  spec.add_development_dependency "bundler", "~> 1.10"
-  spec.add_development_dependency "rake", "~> 10.0"
+  spec.add_development_dependency "bundler", "~> 1.16.6"
+  spec.add_development_dependency "rake", "~> 12.0.0"
 end
--- lib/kisi/client.rb
+++ lib/kisi/client.rb
@@ -1,24 +1,112 @@
 require "kisi/client/version"
-require 'httparty'
+require "httparty"
+require "rest_client"
 
 module Kisi
   class Client
     include HTTParty
-    base_uri 'https://api.getkisi.com'
+
+    debug_output STDOUT
+    # @debug = false
     attr_reader :token
 
     def initialize(token)
       @token = token
+      #base_uri "https://api.getkisi.com"
+      @base_uri = "https://private-anon-078fb5ecdb-kisiapi.apiary-mock.com"
+      #@debug = true # todo remove for production
+      #set_debug_url
+      @headers = {
+        :content_type => "application/json",
+        :accept => "application/json",
+        "Authorization" => "KISI-LOGIN #{@token}",
+      }
     end
 
-    def get_doors
-      self.class.get("/locks/index_with_token.json?limit=10&offset=0&token=#{token}")
+    def debug_enabled(is_enabled = true)
+      @debug = is_enabled
+      #set_debug_url
     end
 
-    def unlock_door(door)
+    def set_debug_url
+      if @debug
+        @base_uri = "https://private-anon-078fb5ecdb-kisiapi.apiary-mock.com"
+      end
+    end
+
+    def add_headers(options)
+      # Content-Type:application/json
+      # Accept:application/json
+      # Authorization:KISI-LOGIN <Token>
+      options = options.merge!({
+        headers: {
+          "Authorization": "KISI-LOGIN #{@token}",
+          "Content-Type": "application/json",
+          "Accept": "application/json",
+        },
+      })
+      return options
+    end
+
+    def get_users(options: {})
+      self.class.get("/user", add_headers(options)).parsed_response
+    end
+
+    def create_user(name:, email:, password:, options: {})
+      #   "name": "First Last",
+      # "email": "email@domain.com",
+      # "password": "test1234",
+      # "terms_and_conditions": true
+      # todo password must be 8 characters!
+      # new_options = options.merge!({
+      #   body: {
+      #     user: {
+      #       name: name,
+      #       email: email,
+      #       password: password,
+      #       terms_and_conditions: true,
+      #     },
+      #   }.to_json,
+      # })
+      # puts "OPTIONS!!!: " + options.to_s
+      # # self.class.post("/users/#{signup_token}/sign_up", add_headers(options)).parsed_response
+      # new_options = add_headers(new_options)
+      # puts "NEW OPTIONS!!" + new_options.to_s
+      # self.class.post("/users/sign_up", new_options).parsed_response
+
+      body = {
+        user: {
+          email: email,
+          name: name,
+          password: password,
+          terms_and_conditions: true,
+        },
+      }.to_json
+
+      url = "#{@base_uri}/users/sign_up"
+      puts url
+      response = RestClient.post url, body, @headers
+      puts response
+      return response
+    end
+
+    def get_groups(options: {})
+      self.class.get("/groups", add_headers(options)).parsed_response
+    end
+
+    def get_places(options: {})
+      self.class.get("/places", add_headers(options)).parsed_response
+    end
+
+    def get_locks(options: {})
+      self.class.get("/locks", add_headers(options)).parsed_response
+    end
+
+    def unlock_lock(lock)
+      # not ready
       self.class.post(
-        "/locks/#{door["id"]}/access_with_token.json?token=#{token}",
-        { body: { lock: door } }
+        "/locks/#{lock["id"]}/unlock",
+        {body: {lock: lock}}
       )
     end
   end
